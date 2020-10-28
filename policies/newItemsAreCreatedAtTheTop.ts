6961464f8ace28a41e7d48d6766d5b52dfd8172c
Maarten Nieber <hallomaarten@yahoo.com>
2020-10-28 01:06:50 +0100

    SQ

--- policies/newItemsAreCreatedAtTheTop.ts
+++ policies/newItemsAreCreatedAtTheTop.ts
@@ -4,7 +4,12 @@ import { Addition } from "../facets/Addition";
 import { topOfTheList } from "../policies/DragSourceFromNewItem";
 
 export const newItemsAreCreatedAtTheTop = (ctr: any) => {
-  listen(Addition.get(ctr), "add", function (data: any) {
-    Addition.get(ctr).parentId = topOfTheList;
-  });
+  listen(
+    Addition.get(ctr),
+    "add",
+    function (data: any) {
+      Addition.get(ctr).parentId = topOfTheList;
+    },
+    { after: false }
+  );
 };
