c080d13649075387e580ab50c996f700b9d3ef5f
Maarten Nieber <hallomaarten@yahoo.com>
2020-10-28 11:50:15 +0100

    Sync item with url using highlight instead of selection

--- policies/newItemsAreEdited.ts
+++ policies/newItemsAreEdited.ts
@@ -2,14 +2,9 @@ import { listen } from "facet";
 
 import { Addition } from "../facets/Addition";
 import { Editing } from "../facets/Editing";
-import { Selection } from "../facets/Selection";
 
-export const newItemsAreSelectedAndEdited = (ctr: any) => {
+export const newItemsAreEdited = (ctr: any) => {
   listen(Addition.get(ctr), "add", function (data: any) {
-    const selection = Selection.get(ctr);
-    selection.selectItem({
-      itemId: Addition.get(ctr).item.id,
-    });
     Editing.get(ctr).setIsEditing(true);
   });
   listen(Addition.get(ctr), "cancel", function (data: any) {
