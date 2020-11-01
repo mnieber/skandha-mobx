00ada90cf5e33083bad372945db405ca0e0ad6d3
Maarten Nieber <hallomaarten@yahoo.com>
2020-11-01 13:19:16 +0100

    Use actions in Insertion

--- facets/Insertion.ts
+++ facets/Insertion.ts
@@ -1,6 +1,5 @@
 import { observable } from "mobx";
-import { input, operation } from "facet";
-import { installHandlers } from "../lib/install";
+import { input, operation, exec } from "facet";
 import { getPreview } from "../internal/getPreview";
 import { mapData } from "..";
 
@@ -17,32 +16,22 @@ export type DragT = DropPositionT & {
 
 export class Insertion {
   @observable @input inputItems?: Array<any>;
-  @operation insertItems(drag: DragT) {}
-
-  static get = (ctr: any): Insertion => ctr.insertion;
-}
-
-type InsertItemsT = (drag: DragT, preview: any[]) => any;
-
-const _handleInsertItems = (insertItems: InsertItemsT) =>
-  function (this: Insertion, drag: DragT) {
+  @operation insertItems(drag: DragT) {
     if (this.inputItems) {
-      const preview = getPreview(
+      const preview: Array<any> = getPreview(
         this.inputItems,
         drag.targetItemId,
         drag.isBefore,
         drag.payload
       );
-      insertItems(drag, preview);
+      exec("insertItems")(preview);
     }
-  };
+  }
 
-interface PropsT {
-  insertItems: InsertItemsT;
+  static get = (ctr: any): Insertion => ctr.insertion;
 }
 
-export const initInsertion = (self: Insertion, props: PropsT) => {
-  installHandlers({ insertItems: _handleInsertItems(props.insertItems) }, self);
+export const initInsertion = (self: Insertion) => {
   return self;
 };
 
