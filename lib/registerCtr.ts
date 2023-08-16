b0a60336e4b778887b164c4b8b5a42f450f619dc
Maarten Nieber <hallomaarten@yahoo.com>
2023-08-16 11:50:10 +0200

    Use initCtr argument from skandha

--- lib/registerCtr.ts
+++ lib/registerCtr.ts
@@ -1,16 +1,13 @@
 import * as Skandha from 'skandha';
 import { makeCtrObservable } from './makeCtrObservable';
 
-export type PropsT = {
+export type ArgsT = {
   ctr: any;
   options: Skandha.RegisterCtrOptionsT;
   initCtr?: Function;
 };
 
-export const registerCtr = (props: PropsT) => {
-  Skandha.registerCtr(props.ctr, props.options);
-  if (props.initCtr) {
-    props.initCtr(props.ctr);
-  }
-  makeCtrObservable(props.ctr);
+export const registerCtr = (args: ArgsT) => {
+  Skandha.registerCtr(args);
+  makeCtrObservable(args.ctr);
 };
